2e63ad4f1a8fcd835855621f9fea505a6233c8c5
student <student@CR3311i.local>
2016-04-30 19:18:26 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.get('/addUser', function (req, res) {
+   // First read existing users.
+   console.log('aaaaa')
+
+})
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
