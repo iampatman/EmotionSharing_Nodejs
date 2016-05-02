1ae7d303ad839c5f932d28586ec4ca757714d820
unknown <370254726@qq.com>
2016-05-03 02:04:15 +0800

    test

--- server.js
+++ server.js
@@ -239,10 +239,8 @@ app.listen(app.get('port'), function() {
 		if (err) throw err;
 		console.log('Connected to postgres! Getting schemas...');
 		var post  = {username: '1', mobilePhone: '13120862631'};
-		var query = client.query('INSERT INTO t_user(username, mobilePhone) values($1, $2)', [post.username, post.mobilePhone], function(err, result) {
-			if (err) {  console.log('Insert data Error: ' + err.message);  } else {console.log("Insert data succeed")}
-			
-		});
+		var query = client.query('INSERT INTO t_user(username, mobilePhone) values($1, $2)', [post.username, post.mobilePhone])
+
 		console.log(query.sql);
 		
 		client.end()
