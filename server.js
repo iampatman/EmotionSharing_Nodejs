5f3c61d61a1531f3be0a926c83cb017fcecd63f2
unknown <370254726@qq.com>
2016-05-03 14:16:57 +0800

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var fs = require("fs");
+var pg = require('pg');
 
 //define web server
 var app = express();
@@ -92,6 +93,131 @@ app.get('/', function(request, response) {
 	response.send('Hello World!');
 });
 
+//Haijun: Create DB table executions. for internel usage only!!!
+app.get('/setupDB', function(request, response) {
+	//make sure connection could be touched
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if(err) {
+			console.log('Connection Error: ' + err.message);
+			response.end("0");
+			return;
+		}
+		console.log('Connected to postgres! Getting schemas...');
+		//Create user table
+		client.query('CREATE TABLE tt_user (username text, mobilephone text)', function(err1, result) {
+		  if (err1)
+		   { console.error(err1); response.send("Error " + err1); response.end("0"); done(); return}
+		  else
+		   { console.log('User table created!!!') }
+		});
+		
+		//Create activities table
+		client.query('CREATE TABLE tt_activity (longitude text, latitude text, time text, username text, emotionid text, thought text)', 
+		function(err2, result) {
+		  if (err2)
+		   { console.error(err2); response.send("Error " + err2); done(); }
+		  else
+		   { console.log('activities table created!!!') }
+		}); 
+		done()
+	});
+	response.end("1");
+}) 
+ 
+ //Haijun: Cleanup DB table executions. for internel usage only!!!
+app.get('/cleanupDB', function(request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if(err) {
+			console.log('Connection Error: ' + err.message);
+			response.end("0");
+			return;
+		}
+		console.log('Connected to postgres! Getting schemas...');
+		//Create user table
+		client.query('DROP TABLE tt_user', function(err1, result) {
+		  if (err1)
+		   { console.error(err1); response.send("Error " + err1); response.end("0"); done(); return}
+		  else
+		   { console.log('User table deleted!!!') }
+		});
+		//Create activities table
+		client.query('DROP TABLE tt_activity', function(err2, result) {
+		  if (err2)
+		   { console.error(err2); response.send("Error " + err2); done()}
+		  else
+		   { console.log('activities table deleted!!!') }
+		});
+		done()
+
+	});
+	response.end("1");
+})
+
+//Haijun: Create DB 
+function db_addUser(newUser) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
+		var queryText = 'INSERT INTO t_user(username, mobilephone) VALUES($1, $2)'
+		
+		client.query(queryText, [newUser.username, newUser.mobilePhone], function(err, result) {
+			if(err) {console.log("error!" + err.message);}//handle error
+		  else {
+			  console.log("DB - new record added " + JSON.stringify(result))
+		  }
+		});
+		done()
+	})
+}
+
+function db_addActivity(newActivity) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
+		var queryText = 'INSERT INTO t_activity(longitude,latitude,time,username,emotionid,thought) VALUES($1, $2, $3, $4, $5, $6)'
+		
+		client.query(queryText, [newActivity.location.longitude,newActivity.location.latitude,newActivity.time,newActivity.username,newActivity.emotionId,newActivity.thought], function(err, result) {
+			if(err) {console.log("error!" + err.message);}//handle error
+		  else {
+			  console.log("DB - new record added " + JSON.stringify(result))
+		  }
+		});
+		done()
+	})
+}
+
+//Haijun: get a list of all users
+function db_getAllUsers() {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
+		
+		client.query('SELECT * FROM t_user').on('row', function(row) {
+			listUsers.push(JSON.stringify(row))
+			console.log("User table is loading " + JSON.stringify(row));
+		});
+		done()
+	})
+}
+
+function db_getAllActivities() {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');	
+		client.query('SELECT * FROM t_activity').on('row', function(row) {
+			var activity = {
+				location: {longitude: row.longitude, latitude: row.latitude},
+				time: row.time,
+				username: row.username,
+				emotionId: row.emotionId,
+				thought: row.thought
+			}
+			activities.push(activity)
+			console.log("Activity table is loading " + JSON.stringify(row))
+		});
+		done()
+	})
+}
 /*
 
 app.get('/cleardata', function(request, response) {
@@ -150,6 +276,32 @@ app.listen(app.get('port'), function() {
 	
 
 	console.log('Node app is running on port: ', app.get('port'));
+	
+	//haijun: --------------
+	//load all data from DB
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
+		
+		client.query('SELECT * FROM t_user').on('row', function(row) {
+			listUsers.push(JSON.stringify(row))
+			console.log("User table loading " + JSON.stringify(row));
+		});
+		
+		client.query("SELECT * FROM t_activity").on('row', function(row) {
+			var activity = {
+				location: {longitude: row.longitude, latitude: row.latitude},
+				time: row.time,
+				username: row.username,
+				emotionId: row.emotionId,
+				thought: row.thought
+			}
+			activities.push(activity)
+			console.log("Activity table loading " + JSON.stringify(row))
+		});
+		//client.end()
+		done();
+	})
 
 	
 /*
